3f7aaa202cf30dfbba99aec0ab4784be1397b233
max2k1 <max2k1@yandex.ru>
2024-02-19 00:08:44 +0000

    Additional actions added

--- README.md
+++ README.md
@@ -9,27 +9,18 @@ sudo chmod 777 /var/www/NotesApp && \
 git clone https://github.com/max2k1/NotesApp.git /var/www/NotesApp && \
 echo "Init completed"
 ``` 
-2. Create `/var/www/NotesApp/.env` file with something relevant, ex:
+2. Create `/var/www/NotesApp/.env` with some defaults:
 ```angular2html
-cat <<EOF > /var/www/NotesApp/.env
-CACHE_MEMCACHED_SERVERS="localhost:11211"
-# DATABASE_URL='postgresql://notes_app:SecretPass@localhost/notes_db'
-GUNICORN_BIND=127.0.0.1:8000
-EOF
+cd /var/www/NotesApp && \
+flask init-env 
 ```
 4. Install all the dependencies:
 ```angular2html
-python3 -m venv .venv && . .venv/bin/activate && pip install -r requirements.txt
+python3 -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt
 ```
-5. Install postgresql and set up the database (with the relevant password):
-```angular2html
-sudo -iu postgres psql << EOF
-CREATE DATABASE notes_db;
-CREATE USER notes_app WITH PASSWORD 'SecretPass';
-GRANT ALL PRIVILEGES ON DATABASE notes_db to notes_app;
-ALTER DATABASE notes_db OWNER TO notes_app;
-\l
-EOF
+5. Install postgresql and set up the database. Generate pgsql initialization script:
+```angular2h
+flask init-pgsql
 ```
 6. Copy systemd unit file into its location and start the service:
 ```angular2html
@@ -39,8 +30,8 @@ sudo systemctl status notes-app
 ```
 7. Install apache2, disable its default site and enable required modules:
 ```angular2html
-sudo a2dissite 000-default.conf
-sudo a2enmod proxy
+sudo a2dissite 000-default.conf && \
+sudo a2enmod proxy && \
 sudo a2enmod proxy_http
 ```
 8. Copy apache2 config to its location and enable it:
@@ -50,9 +41,10 @@ sudo a2ensite NotesApp.conf && \
 sudo systemctl restart apache2 && \
 echo Done
 ```
-9. You can also preseed your database with 10K notes:
+9. You can also pre-seed your database with 10K notes:
 ```angular2html
 pushd /var/www/NotesApp/ && \
+flask recreate-db && \
 flask seed-db && \
 popd && \
 echo "Seeding completed"
--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import os
+import secrets
 import socket
+import string
 from datetime import datetime
+from typing import Any, AnyStr, List, Optional
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, Response, redirect
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+basedir: AnyStr = os.path.abspath(os.path.dirname(__file__))
+env_file: str = os.path.join(basedir, '.env')
+load_dotenv(env_file)
 
 app = Flask(__name__)
 app.config['CACHE_DEFAULT_TIMEOUT'] = int(os.environ.get('CACHE_DEFAULT_TIMEOUT', '10'))
 app.config['CACHE_MEMCACHED_SERVERS'] = os.environ.get('CACHE_MEMCACHED_SERVERS')
 app.config['NOTES_TO_DISPLAY'] = int(os.environ.get('NOTES_TO_DISPLAY', '20'))
+app.config['STATIC_HOSTNAME'] = os.environ.get('STATIC_HOSTNAME')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -32,7 +37,7 @@ with app.app_context():
 
 cache = Cache()
 if app.config['CACHE_MEMCACHED_SERVERS']:
-    cache_servers = app.config['CACHE_MEMCACHED_SERVERS'].split(',')
+    cache_servers: List = app.config['CACHE_MEMCACHED_SERVERS'].split(',')
     cache_config = {"CACHE_TYPE": "memcached",
                     "CACHE_MEMCACHED_SERVERS": cache_servers
                     }
@@ -40,16 +45,22 @@ if app.config['CACHE_MEMCACHED_SERVERS']:
     cache.clear()
 
 
+def hostname() -> str:
+    if app.config['STATIC_HOSTNAME']:
+        return app.config['STATIC_HOSTNAME']
+    return socket.gethostname()
+
+
 @app.route('/', methods=['GET'])
 # We can't cache the whole view:
 # @cache.cached(timeout=50, key_prefix='all_comments')
-def index():
-    server_name = socket.gethostname()
-    notes_to_display = app.config['NOTES_TO_DISPLAY']
+def index() -> str:
+    server_name: str = hostname()
+    notes_to_display: int = app.config['NOTES_TO_DISPLAY']
     cache_key = f"last_{notes_to_display}_notes"
-    notes = None
+    notes: Optional[Note] = None
     cache_configured = True if "cache" in app.extensions else False
-    cached_results = cache.get(cache_key) if cache_configured else None
+    cached_results: Optional[Any] = cache.get(cache_key) if cache_configured else None
     if cached_results:
         notes = cached_results
     else:
@@ -64,15 +75,15 @@ def index():
 
 
 @app.route('/new', methods=['POST'])
-def new():
-    server_name = socket.gethostname()
-    content = request.form['content']
+def new() -> Response:
+    server_name: str = hostname()
+    content: str = request.form['content']
     if content:
         new_note = Note(content=content, server_name=server_name)
         db.session.add(new_note)
         db.session.commit()
 
-        notes_to_display = app.config['NOTES_TO_DISPLAY']
+        notes_to_display: int = app.config['NOTES_TO_DISPLAY']
         cache_key = f"last_{notes_to_display}_notes"
         if "cache" in app.extensions:
             cache.delete(cache_key)
@@ -80,16 +91,57 @@ def new():
     return redirect("/", code=302)
 
 
-@app.cli.command("seed-db")
-def seed_db():
+@app.cli.command("init-env", short_help="Initialize the .env file with some default values")
+def init_env() -> None:
+    if os.path.isfile(env_file):
+        print(f"File {env_file} already exists")
+        return
+    username: str = "notes_app"
+    password: str = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(32))
+    db_name = "notes_db"
+    db_host = "localhost"
+    with open(env_file, "w") as f:
+        f.write(f"DATABASE_USERNAME={username}\n")
+        f.write(f"DATABASE_PASSWORD={password}\n")
+        f.write(f"DATABASE_NAME={db_name}\n")
+        f.write(f"# DATABASE_URL=\"postgresql://{username}:{password}@{db_host}/{db_name}\"\n")
+        f.write("# CACHE_MEMCACHED_SERVERS=localhost:11211\n")
+    print(f"{env_file} file inited")
+
+
+@app.cli.command("init-pgsql", short_help="Create SQL script to initialize PostgreSQL database")
+def init_pgsql() -> None:
+    if not os.path.isfile(env_file):
+        print(f"File {env_file} should exist")
+        return
+    username: str = os.environ.get("DATABASE_USERNAME")
+    password: str = os.environ.get("DATABASE_USERNAME")
+    db_name: str = os.environ.get("DATABASE_NAME")
+    print("sudo -iu postgres psql << EOF")
+    print(f"CREATE DATABASE {db_name};")
+    print(f"CREATE USER {username} WITH PASSWORD '{password}';")
+    print(f"GRANT ALL PRIVILEGES ON DATABASE {db_name} to {username};")
+    print(f"ALTER DATABASE {db_name} OWNER TO {username};")
+    print("EOF")
+
+
+@app.cli.command("recreate-db", short_help="Drop everything from database")
+def recreate_db() -> None:
     db.drop_all()
     db.create_all()
-    server_name = socket.gethostname()
+    db.session.commit()
+    print("Database reinitialized")
+
+
+@app.cli.command("seed-db", short_help="Generate some random notes")
+def seed_db() -> None:
+    db.create_all()
+    server_name = hostname()
     for i in range(10000):
         new_note = Note(content=f"New note #{i + 1}", server_name=server_name)
         db.session.add(new_note)
     db.session.commit()
-    print("Database initialized")
+    print("Database seeded")
 
 
 if __name__ == '__main__':
